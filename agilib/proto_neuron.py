d9882a7f588eae2e34644013ed7809bbd2cee111
Ruslan <kovalyovruslan@gmail.com>
2023-05-26 00:00:06 +0300

    BaseNeuron

---
+++ agilib/proto_neuron.py
@@ -0,0 +1,21 @@
+class BaseNeuron:
+    """
+    The BaseNeuron serves as a protoneuron, providing foundational functionality and structure for more specialized neuron types.
+    """
+    def __init__(self):
+        """
+        Initialize basic properties of the neuron.
+        """
+        pass
+
+    def forward(self, input):
+        """
+        Define the basic computation for the neuron.
+        
+        Args:
+            input: The input data to the neuron.
+
+        Returns: 
+            The output of the neuron's computation.
+        """
+        pass
